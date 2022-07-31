dcb7bcf8676d26dc67c84a3fcb1b03bab2f65749
abhishek <abhiskg20@gmail.com>
2022-07-31 21:26:58 +0600

    Initial Setup

---
+++ variable.ts
@@ -0,0 +1,36 @@
+// let firstName = "abhi";
+// console.log(firstName);
+// console.log(25);
+// console.log(30);
+// let price1 = "10.5";
+// let price2 = 20;
+// console.log(price1 + price2);
+// console.log(parseFloat(price1));
+
+// let num1 = 0.1;
+// let num2 = 0.2;
+// let sum = num1 + num2;
+// sum = parseFloat(sum.toFixed(1));
+// console.log(sum);
+
+// let fruits = ["Apple", "Banana", "Orange"];
+// fruits.pop();
+// fruits.push("Watermelon");
+
+// let index = fruits.indexOf("Banana");
+// console.log(index);
+// console.log(fruits);
+
+let num = 60;
+
+if (num >= 80) {
+  console.log("A");
+} else if (num >= 60 && num < 80) {
+  console.log("B");
+} else if (num >= 50 && num < 60) {
+  console.log("C");
+} else if (num >= 40 && num < 50) {
+  console.log("D");
+} else if (num <= 39) {
+  console.log("F");
+}
