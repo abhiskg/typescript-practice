a0097697af91660a8b1c53e474474ecca7bb020c
abhishek <abhiskg20@gmail.com>
2022-08-03 22:57:56 +0600

    Function Practiced

---
+++ output/function.js
@@ -0,0 +1,38 @@
+"use strict";
+function multiplicationTable(num) {
+    for (let i = num; i <= num * 10; i += num) {
+        console.log(i);
+    }
+}
+multiplicationTable(13);
+function lowercaseConvert(name) {
+    return name.toLowerCase();
+}
+const output = lowercaseConvert("AbHi");
+console.log(output);
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+console.log(fullName("Abhi", "Shek"));
+function square(input) {
+    return Math.pow(input, 2);
+}
+console.log(square(12));
+let average = 0;
+function make_avg(value) {
+    for (let i = 0; i < value.length; i++) {
+        average = average + value[i];
+    }
+    return average;
+}
+const avg = make_avg([1, 2, 3, 4, 5]);
+console.log(avg);
+function odd_even(num) {
+    if (num % 2 === 0) {
+        return "Even no";
+    }
+    else {
+        return "Odd no";
+    }
+}
+console.log(odd_even(5));
