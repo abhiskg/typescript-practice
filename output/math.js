4446aad3f3b7bd7a1e5cab7df97b3a19893003db
abhishek <abhiskg20@gmail.com>
2022-08-08 11:12:36 +0600

    Math and Swap

---
+++ output/math.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log(Math.pow(2, 3));
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.abs(5 - 8));
+//Math.round() returns the nearest integer
+console.log(Math.round(2.56));
+console.log(Math.round(2.5));
+console.log(Math.round(-2.5));
+// lowest one
+console.log(Math.floor(2.5));
+console.log(Math.floor(-2.5));
+// Largest one
+console.log(Math.ceil(2.5));
+console.log(Math.ceil(2.001));
+console.log(Math.ceil(-2.5));
---
+++ output/temp.js
@@ -0,0 +1,9 @@
+"use strict";
+let first = 5;
+let second = 7;
+//Destructing
+[first, second] = [second, first];
+// const temp = first;
+// first = second;
+// second = temp;
+console.log(first, second);
